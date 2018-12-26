584db75c408b7198618832cddda94ed0ba50c298
Beor18 <fer.ragnar19@gmail.com>
2018-12-26 20:39:00 -0300

    agregado componentes login y registro

---
+++ src/components/Login.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+
+class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            errors: {}
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(e) {
+        this.setState({
+            [e.target.email]: e.target.value
+        })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const user = {
+            email: this.state.email,
+            password: this.state.password,
+        }
+
+        console.log(user);
+    }
+
+    render() {
+        return(
+            <div className="container" style={{ marginTop: '50px', width: '700px'}}>
+            <h2 style={{marginBottom: '40px'}}>Login</h2>
+            <form onSubmit={ this.handleSubmit }>
+                <div className="form-group">
+                    <input
+                    type="email"
+                    placeholder="Email"
+                    className="form-control"
+                    name="email"
+                    onChange={ this.handleInputChange }
+                    value={ this.state.email }
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                    type="password"
+                    placeholder="Password"
+                    className="form-control"
+                    name="password"
+                    onChange={ this.handleInputChange }
+                    value={ this.state.password }
+                    />
+                </div>
+                <div className="form-group">
+                    <button type="submit" className="btn btn-primary">
+                        Ingresar
+                    </button>
+                </div>
+            </form>
+        </div>
+        )
+    }
+}
---
+++ src/components/Register.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+
+class Register extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            password_confirmation: '',
+            errors: {}
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(e) {
+        this.setState({
+            [e.target.email]: e.target.value
+        })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const user = {
+            email: this.state.email,
+            password: this.state.password,
+            password_confirmation: this.state.password_confirmation
+        }
+        console.log(user);
+    }
+
+    render() {
+        return(
+            <div className="container" style={{ marginTop: '50px', width: '700px'}}>
+            <h2 style={{marginBottom: '40px'}}>Registration</h2>
+            <form onSubmit={ this.handleSubmit }>
+               
+                <div className="form-group">
+                    <input
+                    type="email"
+                    placeholder="Email"
+                    className="form-control"
+                    name="email"
+                    onChange={ this.handleInputChange }
+                    value={ this.state.email }
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                    type="password"
+                    placeholder="Password"
+                    className="form-control"
+                    name="password"
+                    onChange={ this.handleInputChange }
+                    value={ this.state.password }
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                    type="password"
+                    placeholder="Confirm Password"
+                    className="form-control"
+                    name="password_confirm"
+                    onChange={ this.handleInputChange }
+                    value={ this.state.password_confirmation }
+                    />
+                </div>
+                <div className="form-group">
+                    <button type="submit" className="btn btn-primary">
+                        Registrar Usuario
+                    </button>
+                </div>
+            </form>
+        </div>
+        )
+    }
+}
+
+export default Register;
