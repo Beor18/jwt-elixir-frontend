3607acdb2a572850df2fa08f78db218d9b3e08e0
Beor18 <fer.ragnar19@gmail.com>
2018-12-26 22:04:17 -0300

    redux store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import Navbar from './components/Navbar';
 import Register from './components/Register';
@@ -11,16 +13,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 class App extends Component {
   render() {
     return (
-      <Router>
-          <div>
-            <Navbar />
-              <Route exact path="/" component={ Home } />
-              <div className="container">
-                <Route exact path="/register" component={ Register } />
-                <Route exact path="/login" component={ Login } />
-              </div>
-          </div>
-      </Router>
+      <Provider store = { store }>
+        <Router>
+            <div>
+              <Navbar />
+                <Route exact path="/" component={ Home } />
+                <div className="container">
+                  <Route exact path="/register" component={ Register } />
+                  <Route exact path="/login" component={ Login } />
+                </div>
+            </div>
+        </Router>
+      </Provider>
     );
   }
 }
