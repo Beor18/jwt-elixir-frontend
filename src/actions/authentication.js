9923e552a1c541c94768c7c0d19f9ffbe8eef9e2
Beor18 <fer.ragnar19@gmail.com>
2018-12-27 07:59:27 -0300

    agregado actions

---
+++ src/actions/authentication.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { GET_ERRORS } from './types';
+
+export const resgisterUser = (user, history) => dispatch => {
+    axios.post('http//localhost:4000/api/v1/sign_up', user)
+            .then(res => history.push('/login'))
+            .catch(err => {
+                dispatch({
+                    type: GET_ERRORS,
+                    payload: err.response.data
+                });
+            });
+}
+
+export const loginUser = (user) => dispatch => {
+    axios.post('http//localhost:4000/api/v1/sign_in', user)
+            .then(res => {
+                console.log(res.data);
+            })
+            .catch(err => {
+                dispatch({
+                    type: GET_ERRORS,
+                    payload: err.response.data
+                });
+            });
+}
