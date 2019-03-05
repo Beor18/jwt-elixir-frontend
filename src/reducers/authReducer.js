f2f47bfd250ecfe6a68401ec20916087452697a0
Beor18 <fer.ragnar19@gmail.com>
2019-03-05 18:31:06 -0300

    agregado formulario

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,11 +1,12 @@
-import { SET_CURRENT_USER, CARGAR_USUARIO, CARGAR_HOTELES } from '../actions/types';
+import { SET_CURRENT_USER, CARGAR_USUARIO, CARGAR_HOTELES, AGREGAR_HOTEL } from '../actions/types';
 import isEmpty from '../validation/is-empty';
 
 const initialState = {
     isAuthenticated: false,
     user: {},
     users: {},
-    hotel: {}
+    hotel: {},
+    hoteles: {}
 }
 
 export default function(state = initialState, action ) {
@@ -38,7 +39,19 @@ export default function(state = initialState, action ) {
                     }
                 )
             }
-        
+        case AGREGAR_HOTEL:
+        console.log(action.payload)
+        let nuevoHotel = action.payload
+        let oldState = state.hoteles.slice(0)
+            return { 
+                ...state = 
+                Object.assign({},
+                    state, {
+                        isAuthenticated: !isEmpty(action.payload),
+                        hoteles: [...oldState, nuevoHotel]
+                    }
+                )
+            }
         default: 
             return state;
     }
